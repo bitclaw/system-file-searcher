5c5400d8522815be604871d50bdc8231b8c9df01
Daniel C <bitclaw@gmail.com>
2016-03-16 12:18:59 -0500

    Remove new keyword since FileObject is now a case class and these come built-in with their own companion object

--- src/test/scala/fileSearcher/FilterCheckerTests.scala
+++ src/test/scala/fileSearcher/FilterCheckerTests.scala
@@ -1,6 +1,7 @@
 package fileSearcher
 
 import org.scalatest.FlatSpec
+import java.io.File
 
 /**
  * Test class for System File Searcher implementation
@@ -8,16 +9,16 @@ import org.scalatest.FlatSpec
 class FilterCheckerTests extends FlatSpec{
 
   "FilterCheckerTests passed a list where one file matches the filter" should
-  "return a list with that file" in {
-    val matchingFile = new FileObject("match")
-    val listOfFiles = List(new FileObject("random"), matchingFile)
+    "return a list with that file" in {
+    val matchingFile = FileObject(new File("match"))
+    val listOfFiles = List(FileObject(new File("random")), matchingFile)
     val matchedFiles = FilterChecker("match") findMatchedFiles listOfFiles
     assert(matchedFiles == List(matchingFile))
   }
 
   "FilterCheckerTests passed a list with a directory that matches the filter" should
     "should not return the directory" in {
-    val listOfIOObjects = List(new FileObject("random"),new DirectoryObject("match"))
+    val listOfIOObjects = List(FileObject(new File("random")),new DirectoryObject(new File("match")))
     val matchedFiles = FilterChecker("match") findMatchedFiles listOfIOObjects
     assert(matchedFiles.length == 0)
   }
