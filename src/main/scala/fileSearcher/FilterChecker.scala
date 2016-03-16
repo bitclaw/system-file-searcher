76705af13d1186b651f291d7baaab7034d1cc9eb
Daniel C <bitclaw@gmail.com>
2016-03-16 11:41:04 -0500

    Remove unnecessary brackets since findMatchedFiles definition is really just an expression

--- src/main/scala/fileSearcher/FilterChecker.scala
+++ src/main/scala/fileSearcher/FilterChecker.scala
@@ -10,9 +10,17 @@ class FilterChecker(filter : String) {
 
   def matches(content : String) = content.contains(filter)
 
-  def findMatchedFiles(fileObjects : List[FileObject]) = {
-    for(fileObject <- fileObjects
-        if(matches(fileObject.name)))
-      yield fileObject
+  def findMatchedFiles(IOObjects : List[IOObject]) =
+    for(iOObject <- IOObjects
+        if(iOObject.isInstanceOf[FileObject])
+        if(matches(iOObject.name)))
+      yield iOObject
+
+
+  /**
+   * This is a companion object since it has the same name as an existing class
+   */
+  object FilterChecker {
+    def apply(filter : String) = new FilterChecker(filter)
   }
 }
